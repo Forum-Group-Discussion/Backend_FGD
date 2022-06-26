f0ad483a773f35de3ed8ec9fdaa4cbc0f5c0d900
Razdan12 <90949273+Razdan12@users.noreply.github.com>
2022-06-26 20:49:35 +0700

    first commit feature like comment

---
+++ src/main/java/com/capstone/fgd/domain/dao/LikeComment.java
@@ -0,0 +1,41 @@
+package com.capstone.fgd.domain.dao;
+
+import com.capstone.fgd.domain.common.BaseDao;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+@Table(name = "m_likecomment")
+public class LikeComment extends BaseDao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment commentLike;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users userLike;
+
+    @Column(name = "is_like")
+    private Boolean isLike;
+
+    @Column(name = "is_dislike")
+    private Boolean isDislike;
+}
