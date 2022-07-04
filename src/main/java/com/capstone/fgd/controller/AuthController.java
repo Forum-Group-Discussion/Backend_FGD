c63d068c270d9cf97ad9f55a286a71e9c4d7f123
hafidzencis <hafidzfebrian21@gmail.com>
2022-07-04 14:08:08 +0700

    load image user

--- src/main/java/com/capstone/fgd/controller/AuthController.java
+++ src/main/java/com/capstone/fgd/controller/AuthController.java
@@ -2,8 +2,7 @@ package com.capstone.fgd.controller;
 
 import com.capstone.fgd.domain.dto.UsersRequest;
 import com.capstone.fgd.service.AuthService;
-import com.capstone.fgd.service.FileStorageService;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.capstone.fgd.service.FileService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-import java.io.IOException;
 import java.security.Principal;
 
 @RestController
@@ -23,7 +21,7 @@ import java.security.Principal;
 @RequestMapping(value = "/v1/auth")
 public class AuthController {
     @Autowired
-    private FileStorageService fileStorageService;
+    private FileService fileStorageService;
 
     private final AuthService authService;
 
@@ -32,19 +30,17 @@ public class AuthController {
         return fileStorageService.saveFile(multipartFile,principal);
     }
 
-    @PostMapping(value ="/registerwithuploadfile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value ="/register",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<?> register(@RequestParam("json") String json, @RequestParam(value = "file",required = false) MultipartFile file) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
 
         UsersRequest request = mapper.readValue(json, UsersRequest.class);
-
 
         if (file == null){
             return authService.register(request);
         }
         return authService.registerWithUploadImages(request, file);
     }
-
 
 
     @PostMapping(value  ="/login" )
--- src/main/java/com/capstone/fgd/service/UserService.java
+++ src/main/java/com/capstone/fgd/service/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -144,6 +145,8 @@ public class UserService implements UserDetailsService {
             return ResponseUtil.build(ResponseMessage.KEY_NOT_FOUND,null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
 
 //    public ResponseEntity<Object> deleteUser(Long id){
 //        try {
